38bef1a6d1a1b27ba6a7d6ca5d29bcf4f47ec4a4
Ho-Ya-Ho <rlarkdgh1056@naver.com>
2022-01-24 23:12:48 +0900

    Implement

---
+++ Implement/src/number_4/Main.java
@@ -0,0 +1,32 @@
+//12장 구현(문자열 재정렬)
+
+package number_4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        ArrayList<Character> result = new ArrayList<>();
+        int number =0;
+
+        for(int i =0; i<s.length(); i++){
+            if(Character.isLetter(s.charAt(i))){ //각자리마다 문자인지 판단
+                result.add(s.charAt(i));
+            }
+            else{
+                number +=s.charAt(i) - '0';
+            }
+        }
+        Collections.sort(result); //오름차순 정렬
+
+        for(int i=0; i<result.size(); i++) {
+            System.out.print(result.get(i));
+        }
+        System.out.println(number);
+    }
+}
