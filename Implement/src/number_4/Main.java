594cab329471e29877c1a121da0dcf6696fdfe9b
Ho-Ya-Ho <rlarkdgh1056@naver.com>
2022-01-25 01:47:15 +0900

    Implement

--- Implement/src/number_4/Main.java
+++ Implement/src/number_4/Main.java
@@ -2,9 +2,7 @@
 
 package number_4;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
