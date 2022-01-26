920420be00840faae9514dcb7e37b5d27da6c746
Ho-Ya-Ho <rlarkdgh1056@naver.com>
2022-01-26 23:44:37 +0900

    Binary_Search

---
+++ Binary_Search/src/number_1/Main.java
@@ -0,0 +1,26 @@
+//7장 이진 탐색(떡볶이 떡 만들기)
+
+package number_1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int []rc = new int[n];
+
+        for(int i=0; i< n; i++){
+            rc[i] = sc.nextInt();
+        }
+
+
+
+
+
+    }
+}
